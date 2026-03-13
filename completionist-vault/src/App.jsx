98e0217ba8abe79c48ff8a4cd6866ae1bc71cac0
Monopolizante <robsonlaurentino15@gmail.com>
2026-03-12 22:09:03 -0300

    Adiciona e oculta as chaves de API

--- completionist-vault/src/App.jsx
+++ completionist-vault/src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import axios from "axios";
+import 'dotenv/config';
+import dotenv from 'dotenv';
+dotenv.config();
 
+const API_KEY = process.env.API_KEY;
 function App() {
-
+  const pegarSinonimos = async () => {
+    try{
+      console.log()
+    } catch (error){
+      console.log(error)
+    }}
   return (
     <>  
     </>
