1cdc7d5dc4200fddf5f4035c394b423505becb40
Luca Scharff <lucascharff@proton.me>
2026-04-15 20:46:01 -0300

    Ajeitando o projeto

--- completionist-vault/src/App.jsx
+++ completionist-vault/src/App.jsx
@@ -1,35 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import axios from "axios";
 
-
-const apiPort = 3000
 function App() {
-  const [jogo, setJogo] = useState("")
-  const [conquistas, setConquistas] = useState([])
-  const pegarInformacoes = async () => {
-    try{
-      const response = await axios.get(`http://localhost:${apiPort}/dados/jogo`)
-      const respostaRefinada = response.data.game
-      console.log(response)
-      setJogo(respostaRefinada.gameName)
-      setConquistas(respostaRefinada.availableGameStats.achievements)
 
-    } catch (error){
-      console.log(error)
-    }}
   return (
     <>
       <div>
-        <button onClick={pegarInformacoes}>Teste</button>
-        <p>{jogo}</p>
-        <ul>
-          {conquistas.map((value, index) => (
-            <li>{value.displayName}</li>
-          ))}
-        </ul>
         
-      </div> 
+      </div>
     </>
   )
 }
