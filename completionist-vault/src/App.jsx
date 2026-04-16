e721633c6a2d945885e3d28ed5e618d194834bd1
caioaroliveira-gif <caio_ar_oliveira@estudante.sesisenai.org.br>
2026-04-15 21:50:37 -0300

    Só triteza

--- completionist-vault/src/App.jsx
+++ completionist-vault/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import MainPage from './Pages/MainPage';
 
 function App() {
 
   return (
     <>
       <div>
-        
+        <MainPage />
       </div>
     </>
   )
---
+++ completionist-vault/src/Pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MainPage() {
+    return (
+        <div className="page-main">
+
+        <input type="text"  placeholder="Pesquisar"/> 
+        
+            
+        </div>
+    )
+}
+
+export default MainPage
