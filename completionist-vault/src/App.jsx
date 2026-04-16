2ebddb83586d30255f513b5d935f08ec0ce17fe5
ROBSON LAURENTINO SANT'ANA <robson_l_santana@estudante.sesisenai.org.br>
2026-04-15 21:08:44 -0300

    Adiciona o componente do formulário

--- completionist-vault/src/App.jsx
+++ completionist-vault/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Formulario from './Components/Formulario';
 
 function App() {
 
   return (
     <>
       <div>
-        
+        <Formulario />
       </div>
     </>
   )
