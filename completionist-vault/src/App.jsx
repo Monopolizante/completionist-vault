fe6bd439ad405397d6e4583f931bbcc80f79cd93
Monopolizante <157990766+Monopolizante@users.noreply.github.com>
2026-04-22 22:15:38 -0300

    Revert "Dev (#3)" (#4)
    This reverts commit eb7a9260af7fbd7843d647b07efd202ae9296131.

--- completionist-vault/src/App.jsx
+++ completionist-vault/src/App.jsx
@@ -1,31 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar'; // Componente fixo
+import { useState } from 'react'
+import './App.css'
 import MainPage from './Pages/MainPage';
-import Games from './Pages/Games';
-import Login from './Pages/Login';
-import Cadastro from './Pages/Cadastro';
-import './App.css';
 
 function App() {
-  
-  const [tela, setTela] = useState(<Home />)
-    return (
-      <div className={"div-app"}>
-        <header>
-          <nav>
-          <span class="vaadin--medal"></span>
-          <div className={"nav-bar"}>
-            <button onClick={() => setTela(<Home />)}>Home</button>
-            <button onClick={() => setTela(<Formulario />)}>Formulário</button>
-          </div>
-          </nav>
-        </header>
-        <main className={"main"}>
-          {tela}
-        </main>
-      </div>
 
-    )
+  return (
+    <>
+      <div>
+        <MainPage />
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
--- completionist-vault/src/Pages/MainPage.jsx
+++ completionist-vault/src/Pages/MainPage.jsx
@@ -1,53 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { getGameData } from '../services/api';
-import GameCard from '../components/GameCard';
+import React from 'react'
 
 function MainPage() {
-    const [game, setGame] = useState(null);
-    const [loading, setLoading] = useState(false);
-
-    const handleSearch = async () => {
-        setLoading(true);
-        try {
-            const response = await getGameData();
-            setGame(response.data.game);
-        } catch (error) {
-            console.error("Erro ao buscar dados:", error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
     return (
         <div className="page-main">
-            <header className="steam-header">
-                <input type="text" placeholder="Pesquisar ID do Jogo..." />
-                <button onClick={handleSearch}>Buscar</button>
-            </header>
 
-            <main className="content">
-                {loading && <p>Carregando...</p>}
-                
-                {game && (
-                    <div className="game-info">
-                        <GameCard 
-                            name={game.gameName} 
-                            achievements={game.availableGameStats.achievements} 
-                        />
-                        
-                        <ul className="achievement-list">
-                            {game.availableGameStats.achievements.map((ach, index) => (
-                                <li key={index} className="achievement-item">
-                                    <img src={ach.icon} alt="" />
-                                    <span>{ach.displayName}</span>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
-            </main>
+        <input type="text"  placeholder="Pesquisar"/> 
+        
+            
         </div>
-    );
+    )
 }
 
-export default MainPage;
+export default MainPage
