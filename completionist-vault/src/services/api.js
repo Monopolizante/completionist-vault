523529ff13d680005dd71ad1c4b9a276ac7e123e
Monopolizante <157990766+Monopolizante@users.noreply.github.com>
2026-04-22 22:15:24 -0300

    Revert "Dev (#3)"
    This reverts commit eb7a9260af7fbd7843d647b07efd202ae9296131.

--- completionist-vault/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'http://localhost:3000'
-});
-
-export const getGameData = () => api.get('/dados/jogo');
-
-export default api;
