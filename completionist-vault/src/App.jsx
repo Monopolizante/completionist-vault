fbb02fba0816ac69f000fbf5e61266892438786e
monopolizante <robsonlaurensan@gmail.com>
2026-04-23 21:35:02 -0300

    Conserta o código bugado a um nível utilizável

--- completionist-vault/src/App.jsx
+++ completionist-vault/src/App.jsx
@@ -1,8 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar'; // Componente fixo
+import React from 'react'
+import { useState } from 'react'
 import MainPage from './Pages/MainPage';
 import Games from './Pages/Games';
 import Login from './Pages/Login';
+import Home from './pages/Home'
 import Cadastro from './Pages/Cadastro';
 import './App.css';
 
@@ -13,10 +14,10 @@ function App() {
       <div className={"div-app"}>
         <header>
           <nav>
-          <span class="vaadin--medal"></span>
+          <span className="vaadin--medal"></span>
           <div className={"nav-bar"}>
-            <button onClick={() => setTela(<Home />)}>Home</button>
-            <button onClick={() => setTela(<Formulario />)}>Formulário</button>
+            <button onClick={() => setTela(<MainPage />)}>Página Inicial</button>
+            <button onClick={() => setTela(<Cadastro />)}>Cadastro</button>
           </div>
           </nav>
         </header>
