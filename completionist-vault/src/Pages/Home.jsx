6511a412394471d4fe58a3e2806a24023c08c6d1
Monopolizante <robsonlaurensan@gmail.com>
2026-05-10 19:08:52 -0300

    Adiciona a feature de pegar dados na api na pagina Home, somente testes

--- completionist-vault/src/Pages/Home.jsx
+++ completionist-vault/src/Pages/Home.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from 'react'
-
+import './pages.css'
+import axios from 'axios'
 
 function Home() {
-  return (
-    <div>
-      <h1>The Completionist Vault</h1>
-    </div>
-  )
+    const portaAPI = 3000
+    const pegarDados = async () => {
+        const response = await axios.get(`http://localhost:${portaAPI}/dados/jogo`)
+        console.log(response)
+        setJogo(response.data.game.gameName)
+    }
+    const [jogo, setJogo] = useState()
+    return (
+        <div className="home-main">
+            <h3>Bem vindo ao</h3>
+            <h1>The Completionist Vault</h1>
+            <button onClick={pegarDados}>Clique</button>
+            {jogo}
+        </div>
+    )
 }
 
 export default Home
