4a9ca9736bebb1ee2210c7c5e7a140e645a8a907
monopolizante <robsonlaurentino15@gmail.com>
2026-05-14 19:40:45 -0300

    Adiciona os packages

--- completionist-vault/src/Pages/Login.jsx
+++ completionist-vault/src/Pages/Login.jsx
@@ -2,10 +2,9 @@ import React, { useState } from 'react';
 
 function Login() {
     
-
     return (
         <div className="page-content">
-            <a href="http://localhost:3000/">Login</a>
+            
         </div>
     );
 }
