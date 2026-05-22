36bd725e7a7a665692fa6628a3c85b8661350363
Luck-Css <lucascharff@proton.me>
2026-05-21 21:04:13 -0300

    Cards dos games v0.1

--- completionist-vault/src/Pages/Cards.jsx
+++ completionist-vault/src/Pages/Cards.jsx
@@ -1,31 +1,79 @@
 import './cards.css'
 
 function Cards() {
-  return (
-    <div>
-    <section className='games-list'>
+    return (
+        <div>
+            {/*Card Balatro*/}
+            <section className='games-list'>
 
-      <div className='game-card'>
+                <div className='game-card'>
 
-        <img 
-          src="src/Images/balatro-pc-game-steam-cover.webp" 
-          alt="Balatro" 
-        />
+                    <img
+                        src="src/Images/balatro-pc-game-steam-cover.webp"
+                        alt="Balatro"
+                    />
 
-        <div className='game-info'>
-          <h3>Balatro</h3>
-          <p>Achievements</p>
+                    <div className='game-info'>
+                        <h3>Balatro</h3>
+                        <p>Achievements</p>
 
-          <div className='achievements-total'>
-            <span>5 / 31</span>
-          </div>
+                        <div className='achievements-total'>
+                            <span>5 / 31</span>
+                        </div>
+                    </div>
+
+                </div>
+
+            </section>
+
+            {/*Card Resident Evil 9 Requiem */}
+            <section className='games-list'>
+
+                <div className='game-card'>
+
+                    <img
+                        src="src/Images/resident-evil-requiem-pc-steam-cover.webp"
+                        alt="Resident 9"
+                    />
+
+                    <div className='game-info'>
+                        <h3>Resident Evil Requiem</h3>
+                        <p>Achievements</p>
+
+                        <div className='achievements-total'>
+                            <span>49 / 49</span>
+                        </div>
+                    </div>
+
+                </div>
+
+            </section>
+            {/* Card Pragmata */}
+                        {/*Card Resident Evil 9 Requiem */}
+            <section className='games-list'>
+
+                <div className='game-card'>
+
+                    <img
+                        src="src/Images/pragmata-pc-steam-cover.webp"
+                        alt="Pragmata"
+                    />
+
+                    <div className='game-info'>
+                        <h3>Pragmata</h3>
+                        <p>Achievements</p>
+
+                        <div className='achievements-total'>
+                            <span>0 / 35</span>
+                        </div>
+                    </div>
+
+                </div>
+
+            </section>
+
         </div>
-
-      </div>
-
-    </section>
-    </div>
-  )
+    )
 }
 
 export default Cards
