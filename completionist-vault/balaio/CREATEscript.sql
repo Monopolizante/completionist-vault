d0a4d6afec445d024f8c709cccfc0faabf67b9d1
Monopolizante <robsonlaurentino15@gmail.com>
2026-07-01 19:16:50 -0300

    Adiciona o script de criação do banco de dados a pasta balaio

---
+++ completionist-vault/balaio/CREATEscript.sql
@@ -0,0 +1,15 @@
+/* o nome da database tem que ser completionistVault */
+
+CREATE TABLE IF NOT EXISTS vault_accounts(
+	steam_id BIGINT PRIMARY KEY NOT NULL UNIQUE,
+	email TEXT NOT NULL UNIQUE,
+	password TEXT NOT NULL
+); 
+
+CREATE TABLE IF NOT EXISTS vault_profile(
+	steam_id BIGINT REFERENCES vault_accounts(steam_id),
+	numJogo INT NOT NULL DEFAULT 0,
+	numConquistas INT NOT NULL DEFAULT 0,
+	numHorasJogadas REAL NOT NULL DEFAULT 0.00,
+	vaultPoints INT NOT NULL DEFAULT 0
+);
