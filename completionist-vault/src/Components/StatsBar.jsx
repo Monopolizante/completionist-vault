ee26cd8c244d7eba61c75d5d32054e6647212466
Monopolizante <robsonlaurentino15@gmail.com>
2026-07-02 18:53:08 -0300

    Página stats com os dados pegos da api, funcionando

--- completionist-vault/src/Components/StatsBar.jsx
+++ completionist-vault/src/Components/StatsBar.jsx
@@ -4,18 +4,18 @@ import { useEffect, useState } from "react";
 
 // Puxar lista dos jogos(API version) 
 function StatsBar({games}) {
-  useEffect((games) =>{
-    const pegarFormatarDados = (games) => {
-      console.log(games)
-      const totalPlatinados = games.filter(g => g.total > 0 && g.unlocked === g.total).length
-      const totalUnlocked = games.reduce((acc, g) => acc + (g.unlocked || 0), 0)
-      const totalAchievementsPossiveis = games.reduce((acc, g) => acc + (g.total || 0), 0)
-      const totalHoras = games.reduce((acc, g) => {
-  // Ajeita o 'playtime_forever' que API envia em minutos, converte para horas dividindo por 60
-        const horasNumero = g.playtime_forever ? Math.floor(g.playtime_forever / 60) : 0;
-        return acc + horasNumero}, 0)
-      console.log(totalPlatinados)
-    }}, [])
+  const totalPlatinados = games.filter(g => g.total > 0 && g.unlocked === g.total).length;
+
+  //Cálculo de conquistas totais 
+  const totalUnlocked = games.reduce((acc, g) => acc + (g.unlocked || 0), 0);
+  const totalAchievementsPossiveis = games.reduce((acc, g) => acc + (g.total || 0), 0);
+
+  //Cálculo de horas totais
+  const totalHoras = games.reduce((acc, g) => {
+    // Ajeita o 'playtime_forever' que API envia em minutos, converte para horas dividindo por 60
+    const horasNumero = g.playtime_forever ? Math.floor(g.playtime_forever / 60) : 0;
+    return acc + horasNumero;
+  }, 0);
   //Calculo total de platina (Adaptado para não quebrar tudo enquanto não há dados de conquistas da API osss)
 
   //Cálculo de conquistas totais 
--- completionist-vault/src/Pages/Stats.jsx
+++ completionist-vault/src/Pages/Stats.jsx
@@ -2,21 +2,46 @@
 import { useEffect, useState } from 'react';
 import Navbar from '../Components/Navbar';
 import StatsBar from '../Components/StatsBar';
+import axios from 'axios';
 import "../Styles/pages.css";
 import { useSearchParams } from 'react-router-dom';
 
 function Stats() {
-    const[games, setGames] = useState([])
+    const portaAPI = 3000
+    const[userGames, setUserGames] = useState([])
+    const[loading, setLoading] = useState(true)
     useEffect(() => {
         const pegarDados = async () => {
-            const games = (await axios.get(`http://localhost:${portaAPI}/dados/user/jogos/${userInfo.data.id}`, { withCredentials: true }))
-            console.log(games)
+            try{
+                const userInfo = await axios.get(`http://localhost:${portaAPI}/api/user`, { withCredentials: true });
+                const dados = await axios.get(`http://localhost:${portaAPI}/dados/user/jogos/${userInfo.data.id}`, { withCredentials: true })
+                if (dados.data.jogosUsuario?.response?.games){
+                    const respostaDados = dados.data.jogosUsuario.response.games 
+                    setUserGames(respostaDados)
+                }
+                setLoading(false)
+            } catch (err){
+                console.log(err)
+            }
         }
-    })
+        pegarDados()
+    }, [])
+
+    if (loading) {
+        return (
+          <div className="page">
+            <Navbar />
+            <div className="sync-loading-text">
+              Sincronizando Biblioteca...
+            </div>
+          </div>
+        );
+      }
+
     return (
         <div className='page'>
             <Navbar />
-            <StatsBar games={games}/>
+            <StatsBar games={userGames}/>
         </div>
     )
 }
