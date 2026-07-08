2fb573d660a14f30001cd4f485172c08ebef7d62
CaioRO777 <mamilocremoso235@gmail.com>
2026-07-07 21:47:04 -0300

    Texto da página About inserido.

--- completionist-vault/src/Pages/About.jsx
+++ completionist-vault/src/Pages/About.jsx
@@ -3,11 +3,48 @@ import Navbar from '../Components/Navbar'
 
 function About() {
   return (
-    <div className='page'>
+    <div className="page">
       <Navbar />
-      <h1>Teste About Ossss</h1>
+      <h1>Sobre o projeto</h1>
+      <hr />
+      <p>
+        Bem-vindo ao Completionist Vault Para quem não joga apenas pela
+        história, mas pelo desafio absoluto. Sabe aquela sensação indescritível
+        de ouvir o "ding" do último troféu? Aquele momento em que você olha para
+        a tela após dezenas de horas de grind, derrotando chefes secretos na
+        dificuldade máxima e coletando até o último item escondido no mapa, e
+        finalmente vê a Platina brilhar? Nós também conhecemos essa sensação. E
+        achamos que o seu esforço merece mais do que apenas ficar escondido no
+        menu do seu console. O que é o Completionist Vault? O Completionist
+        Vault é o santuário definitivo para os caçadores de conquistas. Nós
+        nascemos para dar um novo propósito à sua obsessão saudável (ou nem
+        tanto) pelos 100%. Mais do que um agregador de perfis, nós somos o lugar
+        onde o seu suor virtual se transforma em reconhecimento real. Aqui, a
+        sua jornada gamer ganha uma nova camada de recompensa: Organização
+        Suprema: Acompanhe seu progresso, organize sua coleção de troféus e
+        exiba suas platinas em um perfil feito de gamer para gamer. Sua Platina
+        Vale Ouro: Cada conquista desbloqueada nos seus jogos se transforma em
+        pontos na nossa plataforma. Quanto mais raro e difícil o troféu, maior o
+        seu saldo. O Cofre de Cosméticos: Use seus pontos acumulados para abrir
+        o nosso cofre e resgatar cosméticos exclusivos para o site. Personalize
+        seu perfil, ostente insígnias raras e mostre para a comunidade o tamanho
+        do seu legado. Porque terminar a campanha é apenas o começo.
+      </p>
+      <hr />
+      <h1>Sobre a Equipe</h1>
+      <hr />
+      <img src="" alt="É os guri" />
+      <p>Grupo formado por 3 programadores entusiastas.
+        <br />
+        Caio fica só ouvindo o que o Luka e o Robson falam, e não entendi porr nenhuma
+        <img src="" alt="Sabe nada" />
+        <br />
+        Luka, mago do Front-end.
+        <br />
+        Robson, eletriscista do Beck-end (porque para funcionar ele fala que é só com reza braba)
+      </p>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
